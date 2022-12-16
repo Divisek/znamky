e8af7863cbe88ab71dbf637f190380f475bd2412
Divisek <113785426+Divisek@users.noreply.github.com>
2022-12-16 15:41:18 +0100

    Add files via upload

---
+++ znamky.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Z kolika známek chceeč počítat průměr?");
+int pocet_znamek = int.Parse(Console.ReadLine());
+int[] znamky = new int[pocet_znamek]; 
+for(int posun_indexu = 0; posun_indexu < pocet_znamek; posun_indexu++)
+{
+    Console.WriteLine("Zadejte {0}. známku", posun_indexu + 1);
+    znamky[posun_indexu] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Průměr tvých známek je:{0}", znamky.Min());
+Console.ReadKey();
